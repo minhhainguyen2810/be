3113357f7f59e6b43af6bf94e6d788b940ceeca5
minhnguyen1305 <minhhainguyen+1@kms-technology.com>
2019-09-02 18:14:16 +0700

    refactor code

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
-const kittyRoutes = require('./kitty');
-const userRoutes = require('./user');
+const kittyRoutes = require("./kitty");
+const userRoutes = require("./user");
 
 module.exports = function(app) {
-  app.use('/kitty', kittyRoutes);
-  app.use('/user', userRoutes);
+  app.use("/kitty", kittyRoutes);
+  app.use("/user", userRoutes);
 };
--- app/routes/kitty.js
+++ app/routes/kitty.js
@@ -4,31 +4,14 @@ var bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 var Kitten = require("../model/Kitty");
-const jwt = require("express-jwt");
-const jwksRsa = require("jwks-rsa");
 
 router.get("/", (req, res) => {
   Kitten.find((err, kittens) => {
+    const a = "asd";
     if (err) return console.error(err);
     res.status(200).send(kittens);
   });
 });
-
-const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://minhhainguyen.auth0.com/.well-known/jwks.json`
-  }),
-
-  // Validate the audience and the issuer.
-  audience: "https://test-api",
-  issuer: "https://minhhainguyen.auth0.com/",
-  algorithms: ["RS256"]
-});
-
-router.use(checkJwt);
 
 router.post("/", (req, res) => {
   var fluffy = new Kitten({ name: req.body.name });
