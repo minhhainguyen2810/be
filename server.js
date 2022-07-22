927676a7858d079921a695839f2048839b6b18ba
Minh Nguyen <champion.minh@gmail.com>
2022-07-22 09:35:08 +0700

    update origin

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ database.on('error', console.error.bind(console, 'connection error:'))
 database.once('open', function () {
   app.use(
     cors({
-      origin: 'https://minhnguyen-remitano-fe.herokuapp.com/',
+      origin: 'https://minhnguyen-remitano-test-fe.netlify.app/',
       credentials: true,
     })
   )
