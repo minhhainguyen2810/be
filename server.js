6d28bbd614c0fde50a448080e03d1bb4e1a5ebbe
Minh Nguyen <champion.minh@gmail.com>
2022-07-22 10:44:33 +0700

    Update path

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ database.once('open', function () {
     })
   )
   // Serve static files from the React frontend app
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
   // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
   app.get('*', (req, res) => {
